ae769fcede2910852318d72dc391427bd439ee74
Carl <carlst99@outlook.co.nz>
2025-06-19 12:41:06 +1200

    Update JSON context to use string enum converter and ignore null values when writing

--- Mandible.Cli/Objects/CliJsonContext.cs
+++ Mandible.Cli/Objects/CliJsonContext.cs
@@ -2,7 +2,10 @@ using System.Text.Json.Serialization;
 
 namespace Mandible.Cli.Objects;
 
-[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSourceGenerationOptions(
+    WriteIndented = true,
+    UseStringEnumConverter = true,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
 [JsonSerializable(typeof(IndexMetadata))]
 [JsonSerializable(typeof(PackIndex))]
 internal partial class CliJsonContext : JsonSerializerContext
