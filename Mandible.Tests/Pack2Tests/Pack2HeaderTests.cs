ccd4a8dcb44673c470f97294b5d646b9808a159a
Carl <carlst99@outlook.co.nz>
2026-04-11 15:42:37 +1200

    refactor Pack2Header.cs

--- Mandible.Tests/Pack2Tests/Pack2HeaderTests.cs
+++ Mandible.Tests/Pack2Tests/Pack2HeaderTests.cs
@@ -8,18 +8,19 @@ public class Pack2HeaderTests
 {
     private static readonly Pack2Header EXPECTED_HEADER;
     private static readonly byte[] EXPECTED_BYTES =
-    {
-            0x50, 0x41, 0x4b, // PAK
-            0x04, // Version
-            0x01, 0x00, 0x00, 0x00, // Asset count
-            0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Length
-            0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Asset map offset
-            0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Unknown
-            0xff // 'Checksum'
-    };
+    [
+        0x50, 0x41, 0x4b, // PAK
+        0x04, // Version
+        0x01, 0x00, 0x00, 0x00, // Asset count
+        0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Length
+        0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Asset map offset
+        0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Unknown
+        0xff // TRUNCATED 'Checksum'
+    ];
 
     static Pack2HeaderTests()
     {
+        // Allocate space for the checksum, which we don't have in our hardcoded EXPECTED_BYTES
         byte[] bytes = new byte[Pack2Header.Size];
         Buffer.BlockCopy(EXPECTED_BYTES, 0, bytes, 0, EXPECTED_BYTES.Length);
         EXPECTED_BYTES = bytes;
@@ -39,10 +40,7 @@ public class Pack2HeaderTests
         Assert.Equal(EXPECTED_HEADER.Length, header.Length);
         Assert.Equal(EXPECTED_HEADER.AssetMapOffset, header.AssetMapOffset);
         Assert.Equal(EXPECTED_HEADER.Unknown, header.Unknown);
-
-        Assert.Equal(EXPECTED_HEADER.Checksum.Length, header.Checksum.Length);
-        for (int i = 0; i < header.Checksum.Length; i++)
-            Assert.Equal(EXPECTED_HEADER.Checksum.Span[i], header.Checksum.Span[i]);
+        Assert.Equal(EXPECTED_HEADER.Checksum, header.Checksum);
     }
 
     [Fact]
@@ -50,8 +48,6 @@ public class Pack2HeaderTests
     {
         Span<byte> bytes = stackalloc byte[Pack2Header.Size];
         EXPECTED_HEADER.Serialize(bytes);
-
-        for (int i = 0; i < bytes.Length; i++)
-            Assert.Equal(EXPECTED_BYTES[i], bytes[i]);
+        Assert.Equal(EXPECTED_BYTES, bytes);
     }
 }
--- Mandible/Pack2/Pack2Header.cs
+++ Mandible/Pack2/Pack2Header.cs
@@ -1,19 +1,19 @@
-﻿using Mandible.Common;
+﻿using BinaryPrimitiveHelpers;
+using Mandible.Common;
 using Mandible.Exceptions;
 using System;
-using System.Buffers.Binary;
 
 namespace Mandible.Pack2;
 
 /// <summary>
-/// 
+/// Represents a PACK2 header.
 /// </summary>
 /// <param name="AssetCount">Gets the number of assets stored in the pack.</param>
 /// <param name="Length">Gets the length of the pack, in bytes.</param>
 /// <param name="AssetMapOffset">Gets the offset into the pack of the asset map, in bytes.</param>
-/// <param name="Unknown">An unknown value that is always set to 256.</param>
+/// <param name="Checksum">A 128-byte value assumed to be a checksum. How it is calculated is unknown.</param>
 /// <param name="Version">Gets the version represented by this <see cref="Pack2Header"/> object.</param>
-/// <param name="Checksum">A value assumed to be a checksum. How it is calculated is unknown.</param>
+/// <param name="Unknown">An unknown value that is always set to 256.</param>
 public record Pack2Header
 (
     uint AssetCount,
@@ -24,6 +24,8 @@ public record Pack2Header
     ulong Unknown = 256
 )
 {
+    public const int CHECKSUM_LENGTH = 128;
+
     /// <summary>
     /// Gets the magic identifier of a pack2 file.
     /// </summary>
@@ -38,7 +40,7 @@ public record Pack2Header
         + sizeof(ulong) // Length
         + sizeof(ulong) // AssetMapOffset
         + sizeof(ulong) // Unknown
-        + 128; // Checksum
+        + CHECKSUM_LENGTH;
 
     /// <summary>
     /// Serializes this <see cref="Pack2Header"/> to a byte buffer.
@@ -50,15 +52,14 @@ public record Pack2Header
         if (buffer.Length < Size)
             throw new ArgumentException($"Buffer must be at least {Size} bytes", nameof(buffer));
 
-        // Write the magic bytes
-        MAGIC_BYTES.Span.CopyTo(buffer);
-        buffer[3] = Version;
-
-        BinaryPrimitives.WriteUInt32LittleEndian(buffer[4..8], AssetCount);
-        BinaryPrimitives.WriteUInt64LittleEndian(buffer[8..16], Length);
-        BinaryPrimitives.WriteUInt64LittleEndian(buffer[16..24], AssetMapOffset);
-        BinaryPrimitives.WriteUInt64LittleEndian(buffer[24..32], Unknown);
-        Checksum.Span.CopyTo(buffer[32..]);
+        BinaryPrimitiveWriter writer = new(buffer);
+        writer.WriteBytes(MAGIC_BYTES.Span);
+        writer.WriteByte(Version);
+        writer.WriteUInt32LE(AssetCount);
+        writer.WriteUInt64LE(Length);
+        writer.WriteUInt64LE(AssetMapOffset);
+        writer.WriteUInt64LE(Unknown);
+        writer.WriteBytes(Checksum.Span);
     }
 
     /// <summary>
@@ -71,28 +72,17 @@ public record Pack2Header
     /// </exception>
     public static Pack2Header Deserialize(ReadOnlySpan<byte> buffer)
     {
-        int offset = 0;
+        UnrecognisedMagicException.ThrowIfNotAtStart(MAGIC_BYTES.Span, buffer);
 
-        foreach (byte value in MAGIC_BYTES.Span)
-        {
-            if (buffer[offset++] != value)
-                throw new UnrecognisedMagicException(MAGIC_BYTES.ToArray(), buffer[..MAGIC_BYTES.Length].ToArray());
-        }
+        BinaryPrimitiveReader reader = new(buffer);
+        reader.Seek(MAGIC_BYTES.Length);
+        byte version = reader.ReadByte();
+        uint assetCount = reader.ReadUInt32LE();
+        ulong length = reader.ReadUInt64LE();
+        ulong assetMapOffset = reader.ReadUInt64LE();
+        ulong unknown = reader.ReadUInt64LE();
+        ReadOnlySpan<byte> checksum = reader.ReadBytes(CHECKSUM_LENGTH);
 
-        byte version = buffer[offset++];
-
-        uint assetCount = BinaryPrimitives.ReadUInt32LittleEndian(buffer[offset..]);
-        offset += sizeof(uint);
-
-        ulong length = BinaryPrimitives.ReadUInt64LittleEndian(buffer[offset..]);
-        offset += sizeof(ulong);
-
-        ulong assetMapOffset = BinaryPrimitives.ReadUInt64LittleEndian(buffer[offset..]);
-        offset += sizeof(ulong);
-
-        ulong unknown = BinaryPrimitives.ReadUInt64LittleEndian(buffer[offset..]);
-        offset += sizeof(ulong);
-
-        return new Pack2Header(assetCount, length, assetMapOffset, buffer[offset..].ToArray(), version, unknown);
+        return new Pack2Header(assetCount, length, assetMapOffset, checksum.ToArray(), version, unknown);
     }
 }
