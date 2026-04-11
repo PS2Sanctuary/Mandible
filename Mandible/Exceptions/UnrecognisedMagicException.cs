8fad738c3390c7f14a0b81e785585c470910371b
Carl <carlst99@outlook.co.nz>
2026-04-11 15:42:32 +1200

    refactor UnrecognisedMagicException.cs and add UnrecognisedMagicException#ThrowIfNotAtStart

--- Mandible/Exceptions/UnrecognisedMagicException.cs
+++ Mandible/Exceptions/UnrecognisedMagicException.cs
@@ -1,32 +1,48 @@
 using System;
-using System.Collections.ObjectModel;
 
 namespace Mandible.Exceptions;
 
 /// <summary>
-/// Represents an exception that is thrown when the
-/// magic bytes of an input file are not recognised.
+/// Represents an exception that is thrown when a buffer does not contain the expected magic bytes.
 /// </summary>
 public class UnrecognisedMagicException : Exception
 {
     /// <summary>
     /// Gets the magic bytes that were expected.
     /// </summary>
-    public ReadOnlyCollection<byte> ExpectedMagicBytes { get; }
+    public ReadOnlyMemory<byte> ExpectedMagicBytes { get; }
 
     /// <summary>
     /// Gets the magic bytes that were actually read.
     /// </summary>
-    public ReadOnlyCollection<byte> ActualMagicBytes { get; }
+    public ReadOnlyMemory<byte> ActualMagicBytes { get; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="UnrecognisedMagicException"/> class.
     /// </summary>
     /// <param name="expectedMagicBytes">The expected magic bytes.</param>
     /// <param name="actualMagicBytes">The actual magic bytes.</param>
-    public UnrecognisedMagicException(byte[] expectedMagicBytes, byte[] actualMagicBytes)
+    public UnrecognisedMagicException(ReadOnlyMemory<byte> expectedMagicBytes, ReadOnlyMemory<byte> actualMagicBytes)
     {
-        ExpectedMagicBytes = Array.AsReadOnly(expectedMagicBytes);
-        ActualMagicBytes = Array.AsReadOnly(actualMagicBytes);
+        ExpectedMagicBytes = expectedMagicBytes;
+        ActualMagicBytes = actualMagicBytes;
+    }
+
+    /// <summary>
+    /// Throws an <see cref="UnrecognisedMagicException"/> if the given magic bytes are not present at the start of the
+    /// <paramref name="buffer"/>.
+    /// </summary>
+    /// <param name="magicBytes">The magic bytes to detect.</param>
+    /// <param name="buffer">The buffer.</param>
+    /// <exception cref="UnrecognisedMagicException">
+    /// Thrown if the given magic bytes are not present at the start of the <paramref name="buffer"/>.
+    /// </exception>
+    public static void ThrowIfNotAtStart(ReadOnlySpan<byte> magicBytes, ReadOnlySpan<byte> buffer)
+    {
+        if (buffer.StartsWith(magicBytes))
+            return;
+
+        int bufLen = Math.Min(magicBytes.Length, buffer.Length);
+        throw new UnrecognisedMagicException(magicBytes.ToArray(), buffer[..bufLen].ToArray());
     }
 }
