257845e2508a1413be53c54522b574f3607bcb1b
Carl <carlst99@outlook.co.nz>
2026-04-12 23:11:40 +1200

    feat: GnfImage.cs#ReadTextureData

--- Mandible/Gnf/GnfImage.cs
+++ Mandible/Gnf/GnfImage.cs
@@ -1,9 +1,12 @@
 using BinaryPrimitiveHelpers;
+using CommunityToolkit.HighPerformance.Buffers;
 using Mandible.Abstractions.Services;
 using Mandible.Common;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Mandible.Gnf;
 
@@ -25,6 +28,11 @@ public class GnfImage
     public ReadOnlyMemory<byte>? UserData { get; set; }
 
     /// <summary>
+    /// Gets the offset into the file at which the texture data begins.
+    /// </summary>
+    public int TextureDataOffset { get; private set; }
+
+    /// <summary>
     /// Gets the list of textures.
     /// </summary>
     public IReadOnlyList<GnfTextureHeader> Textures { get; private set; }
@@ -39,6 +47,24 @@ public class GnfImage
         LoadFromReader();
     }
 
+    /// <summary>
+    /// Reads a texture's data.
+    /// </summary>
+    /// <param name="textureIndex">The index of the texture header in <see cref="Textures"/>.</param>
+    /// <param name="ct">A <see cref="CancellationToken"/> that can be used to cancel this operation.</param>
+    /// <returns>The texture data.</returns>
+    public async ValueTask<MemoryOwner<byte>> ReadTextureData(int textureIndex, CancellationToken ct = default)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(textureIndex, Textures.Count);
+        GnfTextureHeader header = Textures[textureIndex];
+
+        MemoryOwner<byte> buffer = MemoryOwner<byte>.Allocate((int)header.TextureSize);
+        int address = (int)header.BaseAddress + TextureDataOffset;
+        await _reader.ReadAsync(buffer.Memory, address, ct);
+
+        return buffer;
+    }
+
     [MemberNotNull(nameof(Textures))]
     private void LoadFromReader()
     {
@@ -46,6 +72,7 @@ public class GnfImage
         _reader.Read(headerBuffer, 0);
         BinaryPrimitiveReader reader = new(headerBuffer);
         GnfHeader header = GnfHeader.Deserialize(ref reader);
+        TextureDataOffset = GnfHeader.SIZE + (int)header.ContentsSize;
 
         byte[] contentsBuffer = new byte[(int)header.ContentsSize];
         _reader.Read(contentsBuffer, GnfHeader.SIZE);
