49d0cf6607ffabb8cb708d324d12718468d36f53
Carl <carlst99@outlook.co.nz>
2026-05-02 22:24:22 +1200

    feat: add support for serializing DdsHeader.cs

--- Mandible/Dds/DdsHeader.cs
+++ Mandible/Dds/DdsHeader.cs
@@ -1,4 +1,7 @@
-using System.Runtime.InteropServices;
+using BinaryPrimitiveHelpers;
+using Mandible.Abstractions;
+using System;
+using System.Diagnostics;
 
 namespace Mandible.Dds;
 
@@ -8,8 +11,7 @@ namespace Mandible.Dds;
 /// <remarks>
 /// <seealso href="https://learn.microsoft.com/en-us/windows/win32/direct3ddds/dds-header"/>.
 /// </remarks>
-[StructLayout(LayoutKind.Sequential)]
-public struct DdsHeader
+public struct DdsHeader : IBufferSerializable<DdsHeader>
 {
     public const int SIZE = 124;
 
@@ -63,7 +65,6 @@ public struct DdsHeader
     /// <summary>
     /// Unused.
     /// </summary>
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 11)]
     public uint[] Reserved1;
 
     /// <summary>
@@ -100,5 +101,77 @@ public struct DdsHeader
     {
         Size = SIZE;
         Reserved1 = new uint[11];
+    }
+
+    /// <inheritdoc />
+    public static DdsHeader Read(ReadOnlySpan<byte> buffer, out int amountRead)
+    {
+        BinaryPrimitiveReader reader = new(buffer);
+
+        uint size = reader.ReadUInt32LE();
+        DdsHeaderFlags flags = (DdsHeaderFlags)reader.ReadUInt32LE();
+        uint height = reader.ReadUInt32LE();
+        uint width = reader.ReadUInt32LE();
+        uint pitchOrLinearSize = reader.ReadUInt32LE();
+        uint depth = reader.ReadUInt32LE();
+        uint mipmapCount = reader.ReadUInt32LE();
+        uint[] reserved1 = new uint[11];
+        for (int i = 0; i < 11; i++)
+            reserved1[i] = reader.ReadUInt32LE();
+        DdsPixelFormat pixelFormat = DdsPixelFormat.Deserialize(ref reader);
+        DdsSurfaceFlags caps = (DdsSurfaceFlags)reader.ReadUInt32LE();
+        DdsCubemapFlags caps2 = (DdsCubemapFlags)reader.ReadUInt32LE();
+        uint caps3 = reader.ReadUInt32LE();
+        uint caps4 = reader.ReadUInt32LE();
+        uint reserved2 = reader.ReadUInt32LE();
+
+        amountRead = reader.Offset;
+        return new DdsHeader
+        {
+            Size = size,
+            Flags = flags,
+            Height = height,
+            Width = width,
+            PitchOrLinearSize = pitchOrLinearSize,
+            Depth = depth,
+            MipMapCount = mipmapCount,
+            Reserved1 = reserved1,
+            PixelFormat = pixelFormat,
+            Caps = caps,
+            Caps2 = caps2,
+            Caps3 = caps3,
+            Caps4 = caps4,
+            Reserved2 = reserved2
+        };
+    }
+
+    /// <inheritdoc />
+    public int GetRequiredBufferSize()
+        => SIZE;
+
+    /// <inheritdoc />
+    public int Write(Span<byte> buffer)
+    {
+        BinaryPrimitiveWriter writer = new(buffer);
+
+        writer.WriteUInt32LE(Size);
+        writer.WriteUInt32LE((uint)Flags);
+        writer.WriteUInt32LE(Height);
+        writer.WriteUInt32LE(Width);
+        writer.WriteUInt32LE(PitchOrLinearSize);
+        writer.WriteUInt32LE(Depth);
+        writer.WriteUInt32LE(MipMapCount);
+        foreach (uint val in Reserved1)
+            writer.WriteUInt32LE(val);
+        PixelFormat.Serialize(ref writer);
+        writer.WriteUInt32LE((uint)Caps);
+        writer.WriteUInt32LE((uint)Caps2);
+        writer.WriteUInt32LE(Caps3);
+        writer.WriteUInt32LE(Caps4);
+        writer.WriteUInt32LE(Reserved2);
+
+        Debug.Assert(writer.Offset == SIZE);
+
+        return writer.Offset;
     }
 }
--- Mandible/Dds/DdsPixelFormat.cs
+++ Mandible/Dds/DdsPixelFormat.cs
@@ -1,6 +1,7 @@
+using BinaryPrimitiveHelpers;
+using Mandible.Abstractions;
 using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 namespace Mandible.Dds;
 
@@ -10,8 +11,7 @@ namespace Mandible.Dds;
 /// <remarks>
 /// <seealso href="https://learn.microsoft.com/en-us/windows/win32/direct3ddds/dds-pixelformat"/>
 /// </remarks>
-[StructLayout(LayoutKind.Sequential)]
-public unsafe struct DdsPixelFormat
+public unsafe struct DdsPixelFormat : IBinarySerializable<DdsPixelFormat>
 {
     public static readonly DdsPixelFormat DXT1 = FromFourCC("DXT1"u8);
     public static readonly DdsPixelFormat DXT2 = FromFourCC("DXT2"u8);
@@ -31,7 +31,7 @@ public unsafe struct DdsPixelFormat
 
     /// <summary>
     /// Four-character codes for specifying compressed or custom formats. Possible values include: DXT1, DXT2, DXT3,
-    /// DXT4, or DXT5. A FourCC of DX10 indicates the prescense of the DDS_HEADER_DXT10 extended header, and the
+    /// DXT4, or DXT5. A FourCC of DX10 indicates the presence of the DDS_HEADER_DXT10 extended header, and the
     /// dxgiFormat member of that structure indicates the true format. When using a four-character code,
     /// <see cref="Flags"/> must include <see cref="DdsPixelFormatFlags.DDS_FOURCC"/>.
     /// </summary>
@@ -87,4 +87,46 @@ public unsafe struct DdsPixelFormat
             BBitMask = 0,
             ABitMask = 0
         };
+
+    /// <inheritdoc />
+    public static DdsPixelFormat Deserialize(ref BinaryPrimitiveReader reader)
+    {
+        uint size = reader.ReadUInt32LE();
+        DdsPixelFormatFlags flags = (DdsPixelFormatFlags)reader.ReadUInt32LE();
+        uint fourCC = reader.ReadUInt32LE();
+        uint rgbBitCount = reader.ReadUInt32LE();
+        uint rBitMask = reader.ReadUInt32LE();
+        uint gBitMask = reader.ReadUInt32LE();
+        uint bBitMask = reader.ReadUInt32LE();
+        uint aBitMask = reader.ReadUInt32LE();
+
+        return new DdsPixelFormat
+        {
+            Size = size,
+            Flags = flags,
+            FourCC = fourCC,
+            RgbBitCount = rgbBitCount,
+            RBitMask = rBitMask,
+            GBitMask = gBitMask,
+            BBitMask = bBitMask,
+            ABitMask = aBitMask
+        };
+    }
+
+    /// <inheritdoc />
+    public int GetSerializedSize()
+        => (int)Size;
+
+    /// <inheritdoc />
+    public void Serialize(ref BinaryPrimitiveWriter writer)
+    {
+        writer.WriteUInt32LE(Size);
+        writer.WriteUInt32LE((uint)Flags);
+        writer.WriteUInt32LE(FourCC);
+        writer.WriteUInt32LE(RgbBitCount);
+        writer.WriteUInt32LE(RBitMask);
+        writer.WriteUInt32LE(GBitMask);
+        writer.WriteUInt32LE(BBitMask);
+        writer.WriteUInt32LE(ABitMask);
+    }
 }
